6ff1c6269d1519b61bfbce8e49dbe8411d4725f2
himanshuupadhyay1012 <73900797+himanshuupadhyay1012@users.noreply.github.com>
2020-11-04 08:18:23 +0530

    Update StudentsApplication.java

--- src/main/java/net/techtter/demo/student/StudentsApplication.java
+++ src/main/java/net/techtter/demo/student/StudentsApplication.java
@@ -11,8 +11,7 @@ public class StudentsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentsApplication.class, args);
 
-	 spring.jpa.hibernate.naming.implicit-strategy=org.hibernate.boot.model.naming.ImplicitNamingStrategyLegacyJpaImpl;
-     spring.jpa.hibernate.naming.physical-strategy=org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
+	 
 
 		
 	}
